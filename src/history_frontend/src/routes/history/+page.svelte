ef2d91eaf81df072a1324cc8af1fed00df412fdc
Ayanchi <ayanausub13@gmail.com>
2024-05-10 21:24:56 +0600

    fix

--- src/history_frontend/src/routes/history/+page.svelte
+++ src/history_frontend/src/routes/history/+page.svelte
@@ -3,42 +3,67 @@
   import { onMount } from 'svelte';
   import { backend } from "$lib/canisters";
   import { writable } from 'svelte/store';
-  
+  import searchIcon from '../../assets/searIcon.png'
+
   let files = [];
-  let isLoading = writable(true);
+  let isLoading = writable(false);
+  let search = ''
 
   onMount(async () => {
-    const overviewList = await backend.getOverviewList();
-    let newFiles = [];
+    try {
+      const overviewList = await backend.getOverviewList();
+      let newFiles = [];
 
-    for (const item of overviewList) {
-      const file = await backend.decodeBlobToText(item?.item?.content);
-      let filedata = {
-        file: file, // 'file' is the key, and `file` (variable) is the value
-        createdAt: item?.createdAt 
+      for (const item of overviewList) {
+        const file = await backend.decodeBlobToText(item?.item?.content);
+        let filedata = {
+          file: file,
+          createdAt: item?.createdAt 
+        }
+        newFiles.push(filedata);
       }
-      newFiles.push(filedata);
-    }
 
-    files = newFiles;
+      files = newFiles;
+      isLoading.set(true);
+    } catch (error) {
+      console.log(error);
+      isLoading.set(false);
+    }
     isLoading.set(false);
   });
 
   </script>
   <main>
     <h1>Past History</h1>
-    {#if $isLoading}
+    <div class="flex justify-around items-center">
+      <h2>FILTER</h2>
+
+      <div class="flex">
+        <input id="bill" placeholder="Search Date (May 20, 2020)" bind:value={search} type="text" class="bg-[#C2BE2B] p-4 rounded-2xl w-[300px]"/>
+        <button class="ml-5"><img src={searchIcon} alt="..."/></button>
+      </div>
+    </div>
+    {#if $isLoading === true}
     <p>Loading...</p>
     {:else}
       
-      {#each files as item, i}
-        <AccordionHistory item={item} i={i}/>
-      {/each}
+      {#if files.length > 0}
+        {#each files as item, i}
+          <AccordionHistory item={item} i={i}/>
+        {/each}
+      {:else}
+        <p>No past history available.</p>
+      {/if}
+
     {/if}
   </main>
 
 <style>
   main{
     color: aliceblue;
+  }
+  ::placeholder {
+    color: aliceblue; /* Adjust the color as needed */
+    font-style: italic; /* Adjust other styles as needed */
   }
 </style>
